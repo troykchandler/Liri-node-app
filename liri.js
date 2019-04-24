1060dabe1962132d568c5da8c663a2d1c471289e
Troy Chandler <ttroy225ft@aol.com>
2019-04-23 17:48:36 -0700

    update

--- liri.js
+++ liri.js
@@ -6,41 +6,37 @@ var dateFormat = require("dateFormat")
 var fs = require("fs")
 
 // Takes an artist and searches the Bands in Town 
-// Artist API for an artist and render information
-let concertInfo= function(artist){
+// BandsinTown API for an artist and render information
+let concertThis= function(artist){
     let region = ""
     let queryUrl = "https://rest.bandsintown.com/artists/" + artist.replace(" ", "+") + "/events?app_id=codingbootcamp"
-    //console.log(queryUrl);
     
     request(queryUrl, function(err, response, body){
-        // If the request is successful
         if (!err && response.statusCode === 200) {
-            // Save parsed body in a new variable for easier use
             let concertData = JSON.parse(body)
             
             console.log(artist + " concert information:")
 
-            for (i=0; i < concertInfo.length; i++) {
+            for (i=0; i < concertData.length; i++) {
                 
                 region = concertData[i].venue.region
                 if (region === "") {
-                    region = concertInfo[i].venue.country
+                    region = concertData[i].venue.country
                 }
 
                 // Need to return Name of venue, Venue location, Date of event (MM/DD/YYYY)
                 console.log("Venue: " + concertInfo[i].venue.name)
-                cconsole.log("Location: " + concertInfo[i].venue.city + ", " + region);
-                cconsole.log("Date: " + dateFormat(concertInfo[i].datetime, "mm/dd/yyyy"))
+                console.log("Location: " + concertInfo[i].venue.city + ", " + region);
+                console.log("Date: " + dateFormat(concertInfo[i].datetime, "mm/dd/yyyy"))
             }
         }
     })
 }
 
-// This will take a song, search spotify and return information
-let spotifyThisSong = function(song){
-    // Default should be "The Sign" by Ace of Base
+// This will take a song, search spotify and return information. Default song Buddy Holly 
+let spotifythisSong = function(song){
     if (!song){
-        song = ""
+        song = "Buddy Holly"
     }
 
     let spotify = new Spotify(keys.spotify);
@@ -49,8 +45,6 @@ let spotifyThisSong = function(song){
         if (err) {
             return console.log(err)
         }
-
-        // Need to return Artist(s), Song Name, Album, Preview link of song from Spotify
         let songInfo = data.tracks.items[0]
         console.log(songInfo.artists[0].name)
         console.log(songInfo.name)
@@ -59,19 +53,16 @@ let spotifyThisSong = function(song){
     })
 }
 
-// This will take a movie, search IMDb and return information
-let movieSelection = function(movie){
-    // Default should be "Mr. Nobody"
+// This will take a movie, search IMDb and return information. Defaut Tag.
+let movieThis = function(movie){
     if (!movie){
-        movie = "Mr.+Nobody"
+        movie = "Tag"
     }
 
     let queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    //console.log(queryUrl);
-
-    // Then create a request to the queryUrl
+ 
     request(queryUrl, function(err, response, body){
-        // If the request is successful
+        // If the request is successful then...
         if (!err && response.statusCode === 200) {
             // Need to return: Title, Year, IMDB Rating, Rotten Tomatoes Rating, Country, 
             // Language, Plot, Actors
@@ -90,17 +81,13 @@ let movieSelection = function(movie){
 
 // Using the `fs` Node package, LIRI will take the text inside of random.txt
 // and then use it to call one of LIRI's commands.
-let userAction = function(){
-
-    // read from file
+let doWhatItSays  = function(){
     fs.readFile("random.txt", "utf8", function (err, data) {
         if(err){
-            return console.log(err)
+            return err,
         }
         
         let dataArr = data.split(",")
-
-        // call appropriate function and pass arguement
         runAction(dataArr[0], dataArr[1])
     });
 }
@@ -118,20 +105,20 @@ let userAction = function(){
 
 let runAction = function(func, parm) {
     switch (func) {
-        case "concert-this":
-            concertThis(parm)
+        case "concert-Info":
+         concertThis(parm)
             break
         case "spotify-this-song":
-            spotifyThisSong(parm)
+         spotifythisSong(parm)
             break
-        case "movie-this":
-            movieThis(parm)
+        case "movie-Selection":
+         movieThis(parm)
             break
         case "user-Action":
-            userAction()
+         doWhatItSays()
             break
         default:
-            aler("That is not a command that I recognize, please try again.") 
+            alert("That is not a command that I recognize, please try again.") 
     }
 }
 
